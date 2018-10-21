33d242922476f498bd4ea1b9e4e25fcab2d8ac46
takunnithan <takunnithan@gmail.com>
2018-10-21 19:34:05 +0530

    TODOs

--- backend_api/consumers.py
+++ backend_api/consumers.py
@@ -2,6 +2,31 @@ from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 import json
 
+
+# TODO
+#   1. Redis on 6379 ( may be a docker compose )
+#   2. Dynamic channel & User name
+#   3. Auth on the socket connection
+#   4. re write the consumer to Async ( https://channels.readthedocs.io/en/latest/tutorial/part_3.html )
+#   5. Check Notebook >>>>><<<<<<
+#   6. Websocket client for React ( search npm / write one)
+
+
+# In the django-channel terminology
+
+#   channel -- The client/person connected to the server
+#   group   -- The group clients are listening/connected to
+
+#  Message Flow
+# -------------
+#  Client(channel) connect to server  with a group name
+
+#       They are added to the group
+
+#  Client messages are received in receive()  --> which then send this message to the group
+
+#   Group gets an event and broadcast it to all the clients connected --> `chat_message()`
+
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
         self.room_group_name = 'HGJ87L'     # This is the `general` channel 
